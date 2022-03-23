eaf45cb76101fe1a38f218d057690a34938a260c
DankersW <wouter.dankers@skynet.be>
2022-03-23 11:34:11 +0100

    prj: different builder

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#./scripts/toolchain.sh pdflatex main.tex
+./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf main.tex
 
-rm main.aux
-rm main.log
+#rm main.aux
+#rm main.log
