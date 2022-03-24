d85b74865dfb09a3f126d67ef33a60a9bea11c78
DankersW <wouter.dankers@skynet.be>
2022-03-24 10:15:07 +0100

    docs: added notes

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,7 +2,7 @@
 
 for src_file in "cv" "portfolio"; do
     echo $src_file
-    ./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf $src_file.tex
+    ./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf src/$src_file.tex
     
     for extension in "fdb_latexmk" "fls" "aux" "log"; do
         rm $src_file.$extension
