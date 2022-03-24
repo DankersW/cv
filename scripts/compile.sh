2ee175c88a1805e507136f22e852f96738956a6a
DankersW <wouter.dankers@skynet.be>
2022-03-24 10:06:49 +0100

    toolchain: smarter way of compiling

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf cv.tex
-
-rm cv.fdb_latexmk
-rm cv.fls
-rm cv.aux
-rm cv.log
+for src_file in "cv" "portfolio"; do
+    echo $src_file
+    ./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf $src_file.tex
+    
+    for extension in "fdb_latexmk" "fls" "aux" "log"; do
+        rm $src_file.$extension
+    done
+done
