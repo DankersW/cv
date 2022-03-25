f0913b3029c71bfd9c30f25a1a0355a083a78904
DankersW <wouter.dankers@skynet.be>
2022-03-25 14:50:34 +0100

    prj: using build folder for pdf files

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+mkdir -p build
+
 for src_file in "cv" "portfolio"; do
     echo $src_file
     ./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf src/$src_file.tex
-    
+    mv $src_file.pdf build/
+
     for extension in "fdb_latexmk" "fls" "aux" "log"; do
         rm $src_file.$extension
     done
