75f0dc1bf1e80dfcff060bb653e47cda8bf7b1bc
DankersW <wouter.dankers@skynet.be>
2022-03-25 14:53:54 +0100

    ci: tryout publish folder

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,7 +5,7 @@ mkdir -p build
 for src_file in "cv" "portfolio"; do
     echo $src_file
     ./scripts/toolchain.sh latexmk -cd -f -interaction=batchmode -pdf src/$src_file.tex
-    mv $src_file.pdf build/
+    mv $src_file.pdf build/wouter_dankers_$src_file.pdf
 
     for extension in "fdb_latexmk" "fls" "aux" "log"; do
         rm $src_file.$extension
